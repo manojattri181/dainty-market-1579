7151a3122d438496f5b1178aa64119fab9d1291d
pragati tripathi <mailpragatihere@gmail.com>
2022-09-30 19:09:38 +0530

    Add Project and Add task drawers, sorting menues

--- frontend/src/Pages/PrivatePages/Projects/Sidebar.jsx
+++ frontend/src/Pages/PrivatePages/Projects/Sidebar.jsx
@@ -95,6 +95,7 @@ const Sidebar = () => {
                           value=""
                           id="small-toggle"
                           class="sr-only peer"
+                          checked
                         />
                         <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none  dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
