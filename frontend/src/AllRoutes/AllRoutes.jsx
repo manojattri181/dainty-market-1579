4d97830c4917ec538249f20bced3bac14744437a
manojattri181 <manojattri181@gmail.com>
2022-10-02 17:28:20 +0530

    updates

--- frontend/src/AllRoutes/AllRoutes.jsx
+++ frontend/src/AllRoutes/AllRoutes.jsx
@@ -8,8 +8,8 @@ import Projects from "../Pages/PrivatePages/Projects/Projects";
 import Reports from "../Pages/Reports/Reports";
 import Signup from "../Pages/Signup";
 import Timecards from "../Pages/Features/Timecards";
-import AddTaskModel from "../Pages/hours/weekly-modals/AddTaskModel";
 import Hours from "../Pages/hours/weekly-modals/Hours";
+import Attendance from "../Pages/Features/Attendence";
 
 const AllRoutes = () => {
   return (
@@ -21,7 +21,7 @@ const AllRoutes = () => {
         <Route path="/integrations" element={<Integrations />} />
 
         {/* featurespages */}
-        {/* <Route path="/features/attendancetracking" element={<Attendancetracking />} />  */}
+        <Route path="/features/attendancetracking" element={<Attendance />} /> 
         <Route path="/features/timecards" element={<Timecards />} />
         <Route path="/features/onlinetimesheet" element={<Onlinetimesheet />} />
 
--- frontend/src/Pages/Integrations/Integrations.jsx
+++ frontend/src/Pages/Integrations/Integrations.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import AppGrid from "./AppGrid";
 import TopSection from "./TopSection";
 import TrackSection from "./TrackSection";
@@ -7,7 +8,8 @@ const Integrations = () => {
   document.title = "Time Tracking Integrations - Tracking Time";
   return (
     <>
-      <div className=" lg:ml-44 lg:w-3/4 md:text-center sm:text-center md:w-full m-auto sm:w-full ">
+       <Navbar/>
+       <div className=" lg:ml-44 lg:w-3/4 md:text-center sm:text-center md:w-full m-auto sm:w-full ">
         <TopSection />
         <AppGrid />
         <TrackSection />
